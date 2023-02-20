b69a40f5da6a6dcec95a5c76d8ecfadf26473f72
Technicianjakir <95968477+Technicianjakir@users.noreply.github.com>
2023-02-20 14:56:02 +0600

    Add files via upload

---
+++ ajax.js
@@ -0,0 +1,11 @@
+$(".btn").keyup(function(){
+
+    alert("hi");
+    // $.ajax({
+    //     method:"POST",
+    //     url:"insert.php",
+    //     success:function(data){
+    //     $("#reload").html(data)
+    //     }
+    // });
+});
